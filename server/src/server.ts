0ac9620e9ed9c2bdc981e7071bf0814a6a230e49
agradilenko <44676263+agradilenko@users.noreply.github.com>
2022-12-05 21:46:07 +0300

    Add connection to db

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,25 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import { Application } from 'express';
+import mongoose from 'mongoose';
+import { ConnectionOptions } from 'tls';
 
 const app: Application = express();
+
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.MONGO_URI as string,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectionOptions,
+  )
+  .then(() => console.log('Database connected'))
+  .catch((err) => console.log(err));
 
 app.use(cors());
 app.use(express.json());
